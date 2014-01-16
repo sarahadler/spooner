c2a2b0ac85f8d421704300e712f1c19841c9bc8b
sarah@spoonuniversity.com <sadler@u.northwestern.edu>
2014-01-15 21:29:18 -0500

    changed location of secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,6 +252,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   require "omniauth-facebook"
-  config.omniauth :facebook, "799081603441413", "171717d17cb0c0c8b224656e62533c4e"
-
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
