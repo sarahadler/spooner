1d2bd47bdcb5af8129ee5e436282de6a9348d04a
sarah@spoonuniversity.com <sadler@u.northwestern.edu>
2014-01-17 21:31:22 -0500

    idk

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,4 @@
 class RecipesController < ApplicationController
-  before_filter :authenticate_user!
 
  #        root        /                              recipes#search
  	def search
@@ -7,48 +6,17 @@ class RecipesController < ApplicationController
  	end
 # 			recipes GET   /recipes(.:format)             recipes#index
   def index
-	 	if params[:ingredients] && params[:how]
+	 	if params[:ingredients]
 	 		
 	 		####  MAKE PARAMS USABLE
 
-	 		@ingredients = params[:ingredients]
-	 		array = @ingredients.split(/,|and|&/)
-		 	answer = array.map {|answer| answer.strip}
-		
-			####  IF FILTERING BY 'AND'
-	 		if params[:how] == "and"
-				regex = answer.map { |answer| Regexp.new(answer, 'i') }	
-				@found = {}
-					regex.each do |regex|
-						@found[regex.to_s.to_sym] = []
-						Recipe.all.each do |recipe| 
-							if recipe[:ingredients] =~ regex
-								@found[regex.to_s.to_sym] << recipe
-							end
-						end
-					end
-				@common = @found.values.first
-					@found.values.each do |array| 
-						@common = @common & array
-					end
 
-			####  IF FILTERING BY 'OR'
-
-			elsif params[:how] == "or"
-				answer = answer.join('|')
-				regex = Regexp.new(answer, 'i')
-				
-				@common = []
-				Recipe.all.each do |recipe|
-					if recipe[:ingredients] =~ regex
-						@common << recipe
-					end
-				end
-			end
 
 			####  RETURN RESULTS
 
-			@common
+			@or
+			@and
+			binding.pry
 			@ingredients = array.join(' and ')
 	 	else
 	 		redirect_to root_path
